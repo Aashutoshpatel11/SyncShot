3135e880cbd5abc4aa4aa6ddd510979c69e8611e
Aashutosh Patel <aashutoshpatel1110@gmail.com>
2026-01-02 22:27:16 +0530

    Add API image fetching and React Query provider components

---
+++ src/api.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { UnsplashImage } from "./types";
+
+export const fetchImages = async ({ pageParam = 1 }): Promise<UnsplashImage[]> => {
+
+  const response = await axios.get<UnsplashImage[]>(`https://api.unsplash.com/photos`, {
+    params: { page: pageParam, per_page: 12 },
+    headers: { Authorization: `Client-ID ${NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}` },
+  });
+
+  return response.data;
+}
