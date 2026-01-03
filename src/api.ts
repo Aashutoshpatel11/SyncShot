d4f754b84cf039aad456a125a47142f653868d1b
Aashutosh Patel <aashutoshpatel1110@gmail.com>
2026-01-03 09:56:42 +0530

    Fixed API call and layout structure

--- src/api.ts
+++ src/api.ts
@@ -4,8 +4,9 @@ import { UnsplashImage } from "./types";
 export const fetchImages = async ({ pageParam = 1 }): Promise<UnsplashImage[]> => {
 
   const response = await axios.get<UnsplashImage[]>(`https://api.unsplash.com/photos`, {
+    
     params: { page: pageParam, per_page: 12 },
-    headers: { Authorization: `Client-ID ${NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}` },
+    headers: { Authorization: `Client-ID ${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}` },
   });
 
   return response.data;
